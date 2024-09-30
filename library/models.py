26e331e1b938b0a240071d9c0db9a47493f0739f
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-09-30 15:55:16 +0300

    Миграция 2 и shell+ipython

--- library/models.py
+++ library/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Author(models.Model):
     first_name = models.CharField(max_length=150, verbose_name='Имя')
     last_name = models.CharField(max_length=150, verbose_name='Фамилия')
-    birth_date = models.DateTimeField(verbose_name='Дата рождения')
+    birth_date = models.DateField(verbose_name='Дата рождения')
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
